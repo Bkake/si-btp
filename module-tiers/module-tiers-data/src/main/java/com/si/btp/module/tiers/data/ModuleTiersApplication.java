54fead80da162b8c0902dbb634cff61ce92a1f83
Bkake <bangaly.kake@gmail.com>
2017-11-28 23:14:59 +0100

    Add config module third data

---
+++ module-tiers/module-tiers-data/src/main/java/com/si/btp/module/tiers/data/ModuleTiersApplication.java
@@ -0,0 +1,14 @@
+package com.si.btp.module.tiers.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleTiersApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ModuleTiersApplication.class, args);
+    }
+}
+
+
